9b2457fb718671969e7b9cd800572786bc7e2d5b
Conrado julio <conradojuliosisnero@gmail.com>
2024-05-29 08:54:19 -0500

    dark theme agregado

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,18 @@
+import { FOOTERDATA } from "../../data/inputs";
+import { Link } from "react-router-dom";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="">
+      <div>
+        <ul className="">
+          {FOOTERDATA.map(({ id, name, path }) => {
+            <li className="" key={id}>
+              <Link to={path}>{name}</Link>
+            </li>;
+          })}
+        </ul>
+      </div>
+    </footer>
+  );
 }
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,9 +1,16 @@
-import Login from "./Login/Login";
+// import Login from "./Login/Login";
 
 export default function Index() {
   return (
     <div>
-      <Login/>
+      <div className="flex place-content-center mx-auto mt-4 w-full p-3 h-[100px]">
+        <h1
+          className="font-extrabold text-4xl bg-gradient-to-r from-blue-600 via-blue-400 to-violet-700 bg-clip-text
+        text-transparent md:text-5xl lg:text-6xl"
+        >
+          TodoApp V2
+        </h1>
+      </div>
     </div>
   );
 }
